b8c2c62c45222671f833706b9d0e3165e23c0751
OMER CANGA <ocanga2@sgk.gov.tr>
2019-11-18 17:09:59 +0300

    gene değişti

--- intro/src/main/java/deneme/Deneme.java
+++ intro/src/main/java/deneme/Deneme.java
@@ -3,17 +3,13 @@ package deneme;
 public class Deneme {
 
 public int sayiBul() {
-
+// test işleme devam
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Sayılar Toplamı:" + i);
-			System.out.println("Sayılar Toplamı:" + i);
-			System.out.println("Sayılar Toplamı:" + i);
+		
 		
 		}
-		return 0;//FIX burası düzeltilecek
-		//testt
-		//teste bunuda ekle
-		//FIX
+		return 0;
 
 	}
 
