872d717cb5ea079ca600839fe3b96aaaac3df475
OMER CANGA <ocanga2@sgk.gov.tr>
2019-11-18 15:29:24 +0300

    for dögüsü eklendi sayma için

--- intro/src/main/java/com/abc/intro/App.java
+++ intro/src/main/java/com/abc/intro/App.java
@@ -3,12 +3,30 @@ package com.abc.intro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import deneme.Deneme;
+
+
+
+
+
 @SpringBootApplication
 public class App 
 {
+
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 		System.out.println("Spring Deneme");
 		System.out.println("Spring Deneme2");
+		
+		Deneme dene=new Deneme();
+		System.out.println(dene.sayiBul());
+		
+	
 	}
+	
+	
+	
+
 }
--- intro/src/main/java/deneme/Deneme.java
+++ intro/src/main/java/deneme/Deneme.java
@@ -1,5 +1,15 @@
 package deneme;
 
-public class Deneme {	
+public class Deneme {
+
+public int sayiBul() {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Sayılar Toplamı:" + i);
+			
+		}
+		return 0;
+
+	}
 
 }
